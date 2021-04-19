491a65068242979e4f00d4fb4ebecf65165e8985
mikemnjovu <mikemnjovu@gmail.com>
2021-04-19 21:13:32 +0200

    Add: -y to upgrade

--- docker-install-ubuntu-20-04.sh
+++ docker-install-ubuntu-20-04.sh
@@ -15,11 +15,10 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
-apt-cache policy docker-ce 
+apt-cache policy docker-ce
 #sudo apt install docker-ce -y<
 installFun "docker-ce"
 installFun "docker-compose"
 #sudo apt install docker-compose -y
-#sudo systemctl status docker 
+#sudo systemctl status docker
 sudo usermod -aG docker ubuntu $USERNAME
-
