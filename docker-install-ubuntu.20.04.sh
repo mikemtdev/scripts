928d46e39b839d94cdc95e01f2e17b88122632d0
mikemnjovu <mikemnjovu@gmail.com>
2021-04-19 20:51:44 +0200

    Add: server-init.sh docker-install-unbuntu.20.04.sh

---
+++ docker-install-ubuntu.20.04.sh
@@ -0,0 +1,25 @@
+
+
+
+USERNAME="ubuntu"
+sudo apt upgrade -y
+
+
+function installFun(){
+software=$1
+sudo apt update
+sudo apt install $software -y
+
+}
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce 
+#sudo apt install docker-ce -y<
+installFun "docker-ce"
+installFun "docker-compose"
+#sudo apt install docker-compose -y
+#sudo systemctl status docker 
+sudo usermod -aG docker ubuntu $USERNAME
+
---
+++ server-init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt upgrade
+
+
+# install git
+# sudo chmod +x install git
+# sudo ./
+
+# instll docker
+sudo chmod +x docker-install-ubuntu.20.04.sh
+sudo ./docker-install-ubuntu.20.04.sh
