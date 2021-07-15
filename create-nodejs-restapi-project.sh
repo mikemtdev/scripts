00a4f0a1db2c2298bf0d19b9e61e9e8e249a5681
mikemtdev <mikemnjovu@gmail.com>
2021-07-15 11:04:09 +0200

    add new script

--- create-nodejs-restapi-project.sh
+++ create-nodejs-restapi-project.sh
@@ -2,9 +2,10 @@
 
 
 
-FOLDERNAME=$1
-INSTALL=$2
+# -------------------------------------------
 
+
+#--------------------------------------------
 
 MVCFILESTRUCTURE=('Route' 'Controller' 'Model')
 FILES=('readme.md' 'index.js' 'home-route.js' 'home-controller.js' 'model.js')
@@ -22,8 +23,14 @@ DEVDEP=('@innocells/branch-naming-check'
     'morgan')
 
 
+FOLDERNAME=$1
+INSTALL=$2
+
 mkdir $FOLDERNAME
+sudo chown  -R --verbose $USER:$USER $FOLDERNAME || sudo chown  -R --verbose $USER:$USER ./*
+sudo chmod  -R --verbose 777 $FOLDERNAME || sudo chown  -R --verbose 777 ./*
 cd $FOLDERNAME || return
+sudo yarn init -y
 
 # Make Files
 for FILE in "${FILES[@]}"
@@ -40,34 +47,37 @@ mv home-route.js Route/
 mv home-controller.js Controller/
 mv model.js Model/
 echo " # RestApi Templete" > readme.md
-yarn init -y 
+ 
 
 
 
 
-# for PACKAGE in "${DEPS[@]}"
-# do
-#     sudo yarn add "$PACKAGE" 
+for PACKAGE in "${DEPS[@]}"
+do
+     yarn add "$PACKAGE" || sudo yarn add "$PACKAGE" 
     
-# done
+done
 
-# if [ "$INSTALL" == "dev" ] 
-# then 
-# for PACKAGE in "${DEVDEP[@]}"
-# do
-#     sudo yarn add "$PACKAGE" --dev
-    
-# done
-# fi
-if     [ -n "$(tail -c1 package.json )" ]    # if the file has not a trailing new line.
-then
-       truncate -s-1 package.json            # remove one char as the question request.
-else
-       truncate -s-2 package.json            # remove the last two characters
-       echo "" >> package.json               # add the trailing new line back
+if [ "$INSTALL" == "dev" ] 
+then 
+    for PACKAGE in "${DEVDEP[@]}"
+    do
+        echo "
+        ---------------------------------------
+            Installing DEV-DEP +
+        ---------------------------------------
+        "
+        yarn add "$PACKAGE" --dev ||   sudo  yarn add "$PACKAGE" --dev
+        
+    done
 fi
 
-# cat ../husky.txt >> package.json 
 
 
-
+# if     [ -n "$(tail -c1 package.json )" ]    # if the file has not a trailing new line.
+# then
+#        truncate -s-1 package.json            # remove one char as the question request.
+# else
+#        truncate -s-2 package.json            # remove the last two characters
+#        echo "" >> package.json               # add the trailing new line back
+# fi
